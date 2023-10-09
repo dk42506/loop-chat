440f3c4980b5743586df17324e8bf6983da1ba6d
Dylan <dekeller2024@loyolablakefield.org>
2023-10-09 09:06:02 -0400

    feat: username in nav, create chat

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { getAuth, signOut } from "firebase/auth";
-import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, query, where, getDocs, addDoc } from 'firebase/firestore';
 import app from '../components/firebase';
 
 const auth = getAuth(app);
@@ -8,9 +8,32 @@ const db = getFirestore(app);
 
 export default function Navbar() {
 
+  const [username, setUsername] = useState<string | null>(null);
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [searchResults, setSearchResults] = useState<string[]>([]);
   const searchResultsRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+        if (auth.currentUser) {
+            const userRef = collection(db, 'users');
+            const q = query(userRef, where('uid', '==', auth.currentUser.uid));
+
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+                // Assuming that the field containing the username in your document is 'username'
+                const userDoc = querySnapshot.docs[0];
+                setUsername(userDoc.data().username);
+            }
+        }
+    };
+
+    fetchUsername();
+
+    // If you want to refetch the username when the user changes, 
+    // you can add an auth state changed listener here and call fetchUsername.
+
+  }, []);
 
   useEffect(() => {
     if (searchQuery.trim() === '') {
@@ -50,6 +73,72 @@ export default function Navbar() {
     }
   }, [searchResults]);
 
+  const handleNewChat = async (selectedUser: string) => {
+    const currentUserId = auth.currentUser?.uid;
+
+    // Ensure the current user has a valid user ID
+    if (!currentUserId) {
+        console.error("Current user does not have a valid user ID");
+        return;
+    }
+
+    // Ensure the user doesn't create a chat with themselves
+    if (selectedUser === currentUserId) {
+        console.error("You cannot create a chat with yourself");
+        return;
+    }
+
+    // Check if a chat with these participants already exists
+    const chatsRef = collection(db, 'chats');
+    const q = query(chatsRef, 
+                    where(`participants.${currentUserId}`, '==', true),
+                    where(`participants.${selectedUser}`, '==', true));
+
+    const existingChat = await getDocs(q);
+
+    if (!existingChat.empty) {
+        console.error("A chat between these users already exists.");
+        return;
+    }
+
+    // Define chat participants
+    const participants: Record<string, boolean> = {
+        [currentUserId]: true,  // current user
+        [selectedUser]: true    // clicked user
+    };
+
+    // Add new chat to Firestore database
+    addDoc(chatsRef, {
+        participants: participants,
+        messages: []  // initialize with an empty messages array
+    }).then(() => {
+        console.log("Chat created successfully");
+    }).catch((error) => {
+        console.error("Error creating chat:", error);
+    });
+  }
+
+  useEffect(() => {
+    // Function to handle the click event
+    function handleOutsideClick(event: MouseEvent) {
+        if (searchResultsRef.current && event.target instanceof Node) {
+            // Check if the clicked element is outside the search box and the results
+            if (!searchResultsRef.current.contains(event.target) &&
+                !document.querySelector('.search-bar')?.contains(event.target)) {
+                setSearchResults([]);
+            }
+        }
+    }
+
+    // Add the event listener
+    document.addEventListener('click', handleOutsideClick);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+        document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div className="bg-blue5 flex justify-between items-center p-4 relative">
       {/* Left Section (Search Bar) */}
@@ -72,7 +161,10 @@ export default function Navbar() {
             className="absolute w-40 bg-blue3 border border-gray-200 rounded-lg shadow-lg"
           >
             {searchResults.map((result, index) => (
-              <li key={index} className="py-2 px-4 hover:bg-blue2 hover:text-white cursor-pointer">
+              <li key={index} 
+              className="py-2 px-4 hover:bg-blue2 hover:text-white cursor-pointer"
+              onClick={() => handleNewChat(result)}
+              >
                 {result}
               </li>
             ))}
@@ -80,21 +172,22 @@ export default function Navbar() {
         )}
       </div>
 
-      {/* Right Section (Sign Out Button) */}
-      <div>
-        {/* Sign Out Button */}
-        <button
-          className="bg-blue2 text-white px-4 py-2 rounded-lg hover:bg-opacity-70"
-          onClick={() => {
-            signOut(auth).then(() => {
-              // Sign-out successful.
-            }).catch((error) => {
-              // An error happened.
-            });
-          }}
-        >
-          Sign Out
-        </button>
+      {/* Right Section (Username & Sign Out Button) */}
+      <div className="flex items-center">
+          <span className="mr-6 text-lg font-bold text-white">{username}</span>
+          {/* Sign Out Button */}
+          <button
+            className="bg-blue2 text-white px-4 py-2 rounded-lg hover:bg-opacity-70"
+            onClick={() => {
+              signOut(auth).then(() => {
+                // Sign-out successful.
+              }).catch((error) => {
+                // An error happened.
+              });
+            }}
+          >
+            Sign Out
+          </button>
       </div>
     </div>
   );
