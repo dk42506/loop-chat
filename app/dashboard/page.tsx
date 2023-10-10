2704742100121e46d409432bc00c67a8e0534969
Dylan Keller <dekeller2024@loyolablakefield.org>
2023-10-10 14:02:35 -0400

    feat: added sending and recieving chats

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,13 +3,16 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar';
 import PrivateRoute from '../../components/PrivateRoute';
-import { getFirestore, collection, query, where, onSnapshot, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, query, where, onSnapshot, getDocs, doc, updateDoc } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 import app from '../../components/firebase';
 
 export default function Dashboard() {
     const [chats, setChats] = useState<string[]>([]);
     const [currentUsername, setCurrentUsername] = useState<string | null>(null);
+    const [activeChatUser, setActiveChatUser] = useState<string | null>(null);
+    const [messages, setMessages] = useState<any[]>([]);
+    const [messageInput, setMessageInput] = useState<string>('');
 
     const auth = getAuth(app);
     const db = getFirestore(app);
@@ -52,77 +55,143 @@ export default function Dashboard() {
         return () => unsubscribe();
     }, [currentUsername]);
 
-  const messages = [
-    { text: 'Hello!', sender: 'User1' },
-    { text: 'Hi there!', sender: 'User2' },
-    { text: 'This is a really long message that should wrap to the next line if it exceeds the maximum width.', sender: 'User1' },
-    // Add more chat messages as needed
-  ];
-  return (
-    <PrivateRoute>
-      <div className="bg-blue4 text-white min-h-screen flex flex-col">
-        {/* Navbar */}
-        <Navbar />
+    useEffect(() => {
+      if (!currentUsername || !activeChatUser) return;
+  
+      const chatsRef = collection(db, 'chats');
+      const q1 = query(chatsRef, where('participants', '==', [currentUsername, activeChatUser]));
+      const q2 = query(chatsRef, where('participants', '==', [activeChatUser, currentUsername]));
+  
+      const fetchMessages = async () => {
+          const chatSnapshot1 = await getDocs(q1);
+          const chatSnapshot2 = await getDocs(q2);
+          
+          if (!chatSnapshot1.empty) {
+              setMessages(chatSnapshot1.docs[0].data().messages || []);
+          } else if (!chatSnapshot2.empty) {
+              setMessages(chatSnapshot2.docs[0].data().messages || []);
+          }
+      };
+  
+      fetchMessages();
+  
+      const unsubscribe1 = onSnapshot(q1, snapshot => {
+          if (!snapshot.empty) {
+              setMessages(snapshot.docs[0].data().messages || []);
+          }
+      });
+  
+      const unsubscribe2 = onSnapshot(q2, snapshot => {
+          if (!snapshot.empty) {
+              setMessages(snapshot.docs[0].data().messages || []);
+          }
+      });
+  
+      return () => {
+          unsubscribe1();
+          unsubscribe2();
+      };
+    }, [currentUsername, activeChatUser]);
 
-        {/* Chat Container */}
-        <div className="flex-grow flex">
-          {/* Previous Chats (Left Panel) */}
-          <div className="bg-blue3 w-1/4 p-4">
-            {/* Add your list of previous chats here */}
-            <ul className="space-y-4">
-                {chats.map((chatUser, index) => (
-                    <li key={index} className="text-white text-lg border-t border-blue5 pt-4">{chatUser}</li>
-                ))}
-            </ul>
+    const sendMessage = async () => {
+      if (!messageInput.trim()) return;
+  
+      const chatsRef = collection(db, 'chats');
+      const q1 = query(chatsRef, where('participants', '==', [currentUsername, activeChatUser]));
+      const q2 = query(chatsRef, where('participants', '==', [activeChatUser, currentUsername]));
+  
+      const chatSnapshot1 = await getDocs(q1);
+      const chatSnapshot2 = await getDocs(q2);
+  
+      let chatDoc;
+      if (!chatSnapshot1.empty) {
+          chatDoc = chatSnapshot1.docs[0];
+      } else if (!chatSnapshot2.empty) {
+          chatDoc = chatSnapshot2.docs[0];
+      } else {
+          return;
+      }
+  
+      const updatedMessages = [...chatDoc.data().messages, {
+          text: messageInput,
+          sender: currentUsername
+      }];
+  
+      await updateDoc(doc(db, 'chats', chatDoc.id), { messages: updatedMessages });
+  
+      // Clear the input
+      setMessageInput('');
+    };
 
-          </div>
+    return (
+        <PrivateRoute>
+            <div className="bg-blue4 text-white min-h-screen flex flex-col">
+                {/* Navbar */}
+                <Navbar />
 
-          {/* Active Chat (Right Panel) */}
-          <div className="flex-grow p-4 flex flex-col">
-            {/* Chat Messages */}
-            <div className="bg-blue4 rounded-lg p-4 h-full overflow-y-auto">
-              {messages.map((message, index) => (
-                <div
-                  key={index}
-                  className={`mb-2 ${
-                    message.sender === 'User1' ? 'text-right' : 'text-left'
-                  }`}
-                >
-                  <div
-                    className={`bg-white text-blue5 rounded-lg px-4 py-2 ${
-                      message.sender === 'User1'
-                        ? 'rounded-tr-none mr-auto'
-                        : 'rounded-tl-none ml-auto'
-                    }`}
-                    style={{ maxWidth: '60%', display: 'inline-block' }}
-                  >
-                    {message.text}
-                  </div>
+                {/* Chat Container */}
+                <div className="flex-grow flex">
+                    {/* Previous Chats (Left Panel) */}
+                    <div className="bg-blue3 w-1/4 p-4">
+                        <ul className="space-y-4">
+                            {chats.map((chatUser, index) => (
+                                <li 
+                                    key={index} 
+                                    className={`text-white text-lg border-t border-blue5 pt-4 ${chatUser === activeChatUser ? 'bg-blue2' : ''}`}
+                                    onClick={() => setActiveChatUser(chatUser)}
+                                >
+                                    {chatUser}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    {/* Active Chat (Right Panel) */}
+                    <div className="flex-grow p-4 flex flex-col">
+                        {/* Chat Messages */}
+                        <div className="bg-blue4 rounded-lg p-4 h-full overflow-y-auto">
+                            {messages.map((message, index) => (
+                                <div
+                                    key={index}
+                                    className={`mb-2 ${
+                                        message.sender === currentUsername ? 'text-right' : 'text-left'
+                                    }`}
+                                >
+                                    <div
+                                        className={`bg-white text-blue5 rounded-lg px-4 py-2 ${
+                                            message.sender === currentUsername
+                                                ? 'rounded-tr-none mr-auto'
+                                                : 'rounded-tl-none ml-auto'
+                                        }`}
+                                        style={{ maxWidth: '60%', display: 'inline-block' }}
+                                    >
+                                        {message.text}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Chat Input */}
+                        <div className="mt-auto p-4 bg-blue4">
+                            <div className="flex items-center">
+                                <input
+                                    type="text"
+                                    className="flex-grow px-3 py-2 border rounded-lg text-white bg-blue5 mr-2"
+                                    placeholder="Type your message..."
+                                    value={messageInput}
+                                    onChange={e => setMessageInput(e.target.value)}
+                                />
+                                <button
+                                    className="bg-blue2 text-white px-4 py-2 rounded-lg hover:bg-opacity-70"
+                                    onClick={sendMessage}
+                                >
+                                    Send
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              ))}
             </div>
-
-            {/* Chat Input */}
-            <div className="mt-auto p-4 bg-blue4">
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  className="flex-grow px-3 py-2 border rounded-lg text-white bg-blue5 mr-2"
-                  placeholder="Type your message..."
-                />
-                <button
-                  className="bg-blue2 text-white px-4 py-2 rounded-lg hover:bg-opacity-70"
-                  onClick={() => {
-                    // Handle sending messages
-                  }}
-                >
-                  Send
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </PrivateRoute>
-  )
+        </PrivateRoute>
+    )
 }
